6d337870f6ca5c0f42c0027f546d7a641cd66721
Rohit <rohit@gmail.com>
2023-01-10 20:04:31 +0530

    Hello

---
+++ src/main/java/com/example/indigonms/IndigonMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.indigonms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndigonMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndigonMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indigonms/IndigonMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indigonms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndigonMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
